28ce097f438fbc4a0f986dbf180ddba9a3c5923f
Nohelia Yanibelli <Nohelia Yanibelli>
2022-03-04 13:55:55 -0300

    navBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+
+import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
+function NavBar() {
+    return (
+      
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">ItShop!</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">SDD</Nav.Link>
+            <Nav.Link href="#pricing">Tarjetas de video</Nav.Link>
+            <Nav.Link href="#pricing">Monitor</Nav.Link>
+            <Nav.Link href="#pricing">Consolas</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Contacto</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Iniciar Sesión
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+    )
+  }
+  
+  export default NavBar
+  
