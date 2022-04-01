5f3a08ce720129f7fbb46899ef781fa179b6aa3d
Nohelia Yanibelli <Nohelia Yanibelli>
2022-04-01 10:02:52 -0300

    entrega firebase

--- src/container/ItemDetailContainer.jsx
+++ src/container/ItemDetailContainer.jsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import ItemDetail from "../components/ItemDetail/ItemDetail"
 //
-import { gFetch } from "../Utils/gFetch.js"
-//import { getDoc, getFirestore, doc} from 'firebase/firestore'
+//import { gFetch } from "../Utils/gFetch.js"
+import { getDoc, getFirestore, doc} from 'firebase/firestore'
 
 
 function ItemDetailContainer() {
@@ -16,25 +16,25 @@ function ItemDetailContainer() {
 
     const { detailId } = useParams()
 
-    useEffect(() => {
-        gFetch //simulacion de llamado a una api
-
-            .then(resp => setProd(resp.find(prod => prod.id === detailId)))
-            .catch((err) => console.log(err)) // capturamos todos los errores con el catch
-            .finally(() => setLoading(false))//ej loading
-
-    }, [])
-
     // useEffect(() => {
-    //     const db = getFirestore()
-    //     const queryDoc = (doc, 'items', detailId)
-    //     getDoc(queryDoc)
-    //         .then(resp => setProd({ id: resp.id, ...resp.data() }))
+    //     gFetch //simulacion de llamado a una api
+
+    //         .then(resp => setProd(resp.find(prod => prod.id === detailId)))
     //         .catch((err) => console.log(err)) // capturamos todos los errores con el catch
     //         .finally(() => setLoading(false))//ej loading
 
+    // }, [])
 
-    // }, [detailId])
+    useEffect(() => {
+        const db = getFirestore()
+        const queryDoc = doc(db, 'items', detailId)
+        getDoc(queryDoc)
+            .then(resp => setProd({ id: resp.id, ...resp.data() }))
+            .catch((err) => console.log(err)) // capturamos todos los errores con el catch
+            .finally(() => setLoading(false))//ej loading
+
+
+    }, [detailId])
 
     return (
         <>
--- src/container/ItemListContainer.jsx
+++ src/container/ItemListContainer.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import { CardGroup } from "react-bootstrap"
 import ItemList from "../components/ItemList/ItemList"
-import { gFetch } from "../Utils/gFetch.js"
+//import { gFetch } from "../Utils/gFetch.js"
 import { useParams } from "react-router-dom"
-//import { collection, getDocs, getFirestore } from "firebase/firestore"
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore"
+
 
 
 
@@ -16,39 +17,42 @@ function ItemListContainer() {
   const [loading, setLoading] = useState(true)
   const { category } = useParams()
 
+  // useEffect(() => {
+
+
+  //   gFetch //simulacion de llamado a una api
+
+  //     .then(resp => category ? setProds(resp.filter(prod => prod.category === category)) : setProds(resp))
+  //     .catch(err => console.log(err)) // capturamos todos los errores con el catch
+  //     .finally(() => setLoading(false))//ej loading
+
+
+
+
+
+
+  // }, [category])
+
+
   useEffect(() => {
-
-
-    gFetch //simulacion de llamado a una api
-
-      .then(resp => category ? setProds(resp.filter(prod => prod.category === category)) : setProds(resp))
+    const db = getFirestore()
+    const queryCollection = collection(db, 'items')
+    const queryFilter = category? query(queryCollection, where('category',"==", category)): queryCollection 
+    
+    getDocs(queryFilter)
+ //   .then(resp => category ? setProds(resp.filter.map(prod => prod.category === category)) : setProds(resp.docs.map({ id: resp.id, ...resp.data })))
+ 
+     .then(resp => setProds(resp.docs.map(prod=>({ id: prod.id, ...prod.data() }))))
       .catch(err => console.log(err)) // capturamos todos los errores con el catch
       .finally(() => setLoading(false))//ej loading
 
-
-
-
-
-
-  }, [category])
-
-
-//   useEffect(() => {
-//     const db = getFirestore()
-//     const queryCollection = collection(db, 'items')
-//     getDocs(queryCollection)
-//     .then(resp => category ? setProds(resp.map.filter(prod => prod.category === category)) : setProds(resp.docs.map({ id: resp.id, ...resp.data })))
-//     //  .then(resp => setProds(resp.docs.map({ id: resp.id, ...resp.data() })))
-//       .catch(err => console.log(err)) // capturamos todos los errores con el catch
-//       .finally(() => setLoading(false))//ej loading
-
-//  //   { category ? setProds(resp.map.filter(prod => prod.category === category)): setProds(resp.docs.map({ id: resp.id, ...resp.data() })))} 
+ //   { category ? setProds(resp.map.filter(prod => prod.category === category)): setProds(resp.docs.map({ id: resp.id, ...resp.data() })))} 
    
 
 
 
 
-//   }, [category])
+  }, [category])
   return (
 
     <>
